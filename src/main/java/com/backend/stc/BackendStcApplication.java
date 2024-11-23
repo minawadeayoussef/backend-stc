348454710a0ec2e12a6fcb89013a16ec4ed59ae0
mina youssef <myoussef@uplandsoftware.com>
2024-11-23 02:12:49 +0200

    intial

---
+++ src/main/java/com/backend/stc/BackendStcApplication.java
@@ -0,0 +1,13 @@
+package com.backend.stc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendStcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/stc/BackendStcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.stc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendStcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
